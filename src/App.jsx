d9077adf0c62ddecad8c03ebe0f38fe0b4eb1c5c
omnjja <omniahesham910@gmail.com>
2025-09-02 19:17:54 +0300

    initial sturture

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,30 @@
 import "./App.css";
 import { Link, Route, Routes } from "react-router-dom";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
+      <Navbar />
       <nav>
         <Link to="/">Home</Link> | <Link to="/about">About</Link>
       </nav>
       <Routes>
         <Route>
           {/* just testing routes and tailwind */}
-          <Route path="/" element={<h1 className="text-3xl font-bold underline text-amber-900 ">Home Page</h1>} />
+          <Route
+            path="/"
+            element={
+              <h1 className="text-3xl font-bold underline text-amber-900 ">
+                Home Page
+              </h1>
+            }
+          />
           <Route path="/about" element={<h1>About Page</h1>} />
         </Route>
       </Routes>
+      <Footer />
     </>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      bavbar
+    </div>
+  )
+}
+
+export default Navbar
