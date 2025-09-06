0a7d698dae57f9673f7f2eb4117b07cded0f45ea
omnjja <omniahesham910@gmail.com>
2025-09-06 19:26:27 +0300

    fix done

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,11 @@ import "./App.css";
 import { Link, Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
-import LoginForm from "./components/Login";
 
 function App() {
   return (
     <div>
       <Navbar />
-      <LoginForm/>
       <Footer />
     </div>
   );
@@ -16,20 +14,3 @@ function App() {
 
 export default App;
 
-/*      <nav>
-        <Link to="/">Home</Link> | <Link to="/about">About</Link>
-      </nav>
-      <Routes>
-        <Route>
-           just testing routes and tailwind 
-          <Route
-            path="/"
-            element={
-              <h1 className="text-3xl font-bold underline text-amber-900 ">
-                Home Page
-              </h1>
-            }
-          />
-          <Route path="/about" element={<h1>About Page</h1>} />
-        </Route>
-      </Routes> */
