45a5443861b04cac5c56e2fc8e68107721add38b
omnjja <omniahesham910@gmail.com>
2025-09-08 23:14:19 +0300

    edit navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,95 +1,8 @@
-// import React from "react";
-// import style from "../css-modules/Navbar.module.css";
-// import { NavLink } from "react-router-dom";
-
-// const Navbar = () => {
-//   return (
-
-//     <div className={style.navbar}>
-//       <div className="w-[70px] h-[70px] overflow-hidden flex items-center">
-//         <img src="src/public/logo55.png" alt="" className="w-[60px] h-[60px]" />
-//       </div>
-
-//       <div className="flex justify-between w-[500px] font-semibold">
-//         <NavLink
-//           to="/"
-//           className={({ isActive }) => (isActive ? `px-1.5 text-[#FD7E14] border-b-2 border-b-[#FD7E14] border-b-solid` : `px-1.5`)}
-//         >
-//           Home
-//         </NavLink>
-//         <NavLink to="/vets" className={({ isActive }) => (isActive ? `px-1.5 text-[#FD7E14] border-b-2 border-b-[#FD7E14] border-b-solid` : `px-1.5`)}>Vets</NavLink>
-//         <NavLink to="/grooming" className={({ isActive }) => (isActive ? `px-1.5 text-[#FD7E14] border-b-2 border-b-[#FD7E14] border-b-solid` : `px-1.5`)}>Grooming</NavLink>
-//         <NavLink to="/beginner-guide" className={({ isActive }) => (isActive ? `px-1.5 text-[#FD7E14] border-b-2 border-b-[#FD7E14] border-b-solid` : `px-1.5`)}>Beginner Guide</NavLink>
-//       </div>
-
-//       {/* cart  */}
-//       <div className="flex gap-[10px] items-center">
-//         <div className="dropdown dropdown-end">
-//           <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
-//             <div className="indicator">
-//               <svg
-//                 xmlns="http://www.w3.org/2000/svg"
-//                 className="h-5 w-5"
-//                 fill="none"
-//                 viewBox="0 0 24 24"
-//                 stroke="currentColor"
-//               >{" "}
-//                 <path
-//                   strokeLinecap="round"
-//                   strokeLinejoin="round"
-//                   strokeWidth="2"
-//                   d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-//                 />{" "}
-//               </svg>
-//               <span className="badge badge-xs indicator-item bg-[#FD7E14] text-white ">
-//                 0
-//               </span>
-//             </div>
-//           </div>
-//         </div>
-
-//         {/* profile */}
-//         <div className="dropdown dropdown-end">
-//           <div
-//             tabIndex={0}
-//             role="button"
-//             className="btn btn-ghost btn-circle avatar"
-//           >
-//             <div className="w-10 rounded-full">
-//               <img
-//                 alt="Tailwind CSS Navbar component"
-//                 src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-//               />
-//             </div>
-//           </div>
-//           <ul
-//             tabIndex={0}
-//             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
-//           >
-//             <li>
-//               <a className="justify-between">
-//                 Profile
-//                 <span className="badge">New</span>
-//               </a>
-//             </li>
-//             <li>
-//               <a>Logout</a>
-//             </li>
-//           </ul>
-//         </div>
-
-//         <button className={style.btn}>Login</button>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Navbar;
-
 import React, { useState } from "react";
 import style from "../css-modules/Navbar.module.css";
 import { NavLink } from "react-router-dom";
 import { RxHamburgerMenu } from "react-icons/rx";
+import { MdOutlineCancel } from "react-icons/md";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -218,7 +131,7 @@ const Navbar = () => {
       <div className="md:hidden flex items-center">
         <button onClick={() => setIsOpen(!isOpen)} className="text-[#2F4156]">
           {isOpen ? (
-            <RxHamburgerMenu size={28} />
+            <MdOutlineCancel size={28} />
           ) : (
             <RxHamburgerMenu size={28} />
           )}
@@ -227,7 +140,7 @@ const Navbar = () => {
 
       {/* Mobile Menu */}
       {isOpen && (
-        <div className="absolute top-[60px] right-1 w-fit px-3 text-sm bg-[#f5efeb] shadow-lg rounded-b-2xl flex flex-col items-center py-4 space-y-4 md:hidden z-20">
+        <div className="absolute top-[60px] right-0 w-[50%]  px-3  bg-white shadow-lg rounded-b-2xl flex flex-col items-center py-4 space-y-4 md:hidden z-20 ">
           <NavLink
             to="/"
             onClick={() => setIsOpen(false)}
@@ -247,15 +160,43 @@ const Navbar = () => {
             Vets
           </NavLink>
           <NavLink
-            to="/grooming"
+            to="/services"
             onClick={() => setIsOpen(false)}
             className={({ isActive }) =>
               isActive ? "text-[#FD7E14] font-semibold" : "text-[#2F4156]"
             }
           >
-            Grooming
+           Pet Services
+          </NavLink>
+
+          <NavLink
+            to="/profile/info"
+            onClick={() => setIsOpen(false)}
+            className={({ isActive }) =>
+              isActive ? "text-[#FD7E14] font-semibold" : "text-[#2F4156]"
+            }
+          >
+            Profile
           </NavLink>
           <NavLink
+            to="/profile/orders"
+            onClick={() => setIsOpen(false)}
+            className={({ isActive }) =>
+              isActive ? "text-[#FD7E14] font-semibold" : "text-[#2F4156]"
+            }
+          >
+            Orders
+          </NavLink>
+          <NavLink
+            to="/profile/appointments"
+            onClick={() => setIsOpen(false)}
+            className={({ isActive }) =>
+              isActive ? "text-[#FD7E14] font-semibold" : "text-[#2F4156]"
+            }
+          >
+            Appointments
+          </NavLink>
+                    <NavLink
             to="/beginner-guide"
             onClick={() => setIsOpen(false)}
             className={({ isActive }) =>
@@ -264,9 +205,18 @@ const Navbar = () => {
           >
             Beginner Guide
           </NavLink>
+          <NavLink
+            to="/"
+            onClick={() => setIsOpen(false)}
+            className={({ isActive }) =>
+              isActive ? "text-[#FD7E14] font-semibold" : "text-[#2F4156]"
+            }
+          >
+            Signout
+          </NavLink>
 
           {/* mobile login */}
-          <button className={`${style.btn} mt-2`}>Login</button>
+          <button className={`${style.btn} md:hidden`}>Login</button>
         </div>
       )}
     </nav>
