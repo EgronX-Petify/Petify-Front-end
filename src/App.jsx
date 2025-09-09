f0039e62a3b78b65da18fe20143f299323746335
omnjja <omniahesham910@gmail.com>
2025-09-09 17:59:40 +0300

    merge all

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Home from "./pages/Home";
 import Shop from "./pages/Shop";
 import Vets from "./pages/Vets";
 import Grooming from "./pages/PetServices";
+import BeginnerGuide from "./pages/BeginnerGuide";
 
 function App() {
   return (
@@ -15,7 +16,7 @@ function App() {
       <Shop />
       <Vets />
       <Grooming />
-      <BeginnerGuide />
+      <BeginnerGuide/>
       <Footer />
     </div>
   );
